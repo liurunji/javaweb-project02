a74ba18ec7e11723e72e0bee8aa86fcb46909822
liurunji <1486333612@qq.com>
2026-02-08 12:49:12 +0800

    新增全局异常处理器，以及对重复字段的异常处理

---
+++ tlias-web-management/src/main/java/com/itheima/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.itheima.exception;
+
+import com.itheima.pojo.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice   //注解1
+public class GlobalExceptionHandler {
+    @ExceptionHandler  //注解2
+    public Result handleException(Exception e) {
+        log.error("出错了！", e);
+        return Result.error("出错了，请联系管理员");
+    }
+
+    @ExceptionHandler
+    public Result handleDuplicateKeyException(DuplicateKeyException e) {
+        //### Error updating database.  Cause: java.sql.SQLIntegrityConstraintViolationException: Duplicate entry '13309090006' for key 'emp.phone'
+        //从错误信息里提取出电话号码
+        String message = e.getMessage();
+        int i = message.indexOf("Duplicate entry");
+        String s = message.substring(i);
+        String[] arr = s.split(" ");
+        return Result.error(arr[2] + "已存在");
+    }
+}
